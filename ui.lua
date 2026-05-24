c5cd1cf093a0c37b082e344376d95974b82668d3
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-05-24 13:39:43 +1000

    moved ui functions to ui file and added more

---
+++ ui.lua
@@ -0,0 +1,46 @@
+local game = require("game")
+local function formatInventory(inventory)
+	local items = {}
+	for _, item in ipairs(inventory) do
+		table.insert(items, item.name .. " (x" .. item.quantity .. ")")
+	end
+	return table.concat(items, ", ")
+end
+
+local function formatEnemies(enemies)
+	local opponents = {}
+	for _, opponent in ipairs(enemies) do
+		table.insert(opponents, opponent.name .. " " .. opponent.health .. "/" .. opponent.maxHealth)
+	end
+	return table.concat(opponents, "\n")
+end
+local function displayGameState()
+	print("Player Stats:")
+	if game.player.health > 0 then
+		print("  Health: " .. game.player.health .. "/" .. game.player.maxHealth)
+		print("  Inventory: " .. formatInventory(game.player.inventory))
+		print("  Current Room: " .. game.player.currentRoom)
+	else
+		print("You have died")
+	end
+	print("\nGame Stats:")
+	if game.bossBeat == false then
+		print("  Boss Dead: Not yet")
+	else
+		print("  Boss Dead: Yes, Great Work!!")
+	end
+end
+
+local function displayRoomDescription()
+	print(game.rooms[game.player.currentRoom].description)
+end
+
+local function displayEnemies()
+	local enemies = game.rooms[game.player.currentRoom].enemies
+	if #enemies > 0 then
+		print("Enemies:")
+		print(formatEnemies(enemies))
+	else
+		print("No enemies here.")
+	end
+end
