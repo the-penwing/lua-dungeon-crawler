64713694027775669148c49752ea8b3dc16fda84
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-05-26 17:08:39 +1000

    add item ids to items.lua + helper function

--- src/items.lua
+++ src/items.lua
@@ -1,16 +1,29 @@
 local items = {
 	weapons = {
-		rusty_sword = { name = "Rusty Sword", hitChance = 75, damage = 5 },
-		bow = { name = "Bow", hitChance = 80, damage = 10 },
-		longsword = { name = "Longsword", hitChance = 95, damage = 15 },
+		rusty_sword = { name = "Rusty Sword", id = "rustysword", hitChance = 75, damage = 5 },
+		bow = { name = "Bow", id = "bow", hitChance = 80, damage = 10 },
+		longsword = { name = "Longsword", id = "longsword", hitChance = 95, damage = 15 },
 	},
 	consumables = {
-		healing_potion = { name = "Healing Potion", heal = 15 },
-		arrow = { name = "Arrow" },
+		healing_potion = { name = "Healing Potion", id = "healingpotion", heal = 15 },
+		arrow = { name = "Arrow", id = "arrow" },
 	},
 	loot = {
-		gold_coin = { name = "Gold Coin" },
+		gold_coin = { name = "Gold Coin", id = "goldcoin" },
 	},
 }
+local function getItemById(itemId)
+	for _, v in pairs(items) do
+		for _, itemData in pairs(v) do
+			if itemData.id == itemId then
+				return itemData
+			end
+		end
+	end
+	return nil
+end
 
-return items
+return {
+	items,
+	getItemById = getItemById,
+}
