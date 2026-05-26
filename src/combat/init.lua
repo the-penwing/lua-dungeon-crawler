dc9f92ec2c83730a6cc28b3411477e2cee211031
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-05-26 21:25:12 +1000

    combat module init

---
+++ src/combat/init.lua
@@ -0,0 +1,9 @@
+return {
+	actions = require("src.combat.actions"),
+	casting = require("src.combat.casting"),
+	utilise = require("src.combat.utilise"),
+	targeting = require("src.combat.targeting"),
+	enemy = require("src.combat.enemy"),
+	magic = require("src.combat.magic"),
+	loop = require("src.combat.loop"),
+}
