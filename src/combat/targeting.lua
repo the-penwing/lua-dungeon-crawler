94751cb4ae0d2f59e2fe7af6a33a1f75850eaef7
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-05-26 21:06:32 +1000

    targeting

---
+++ src/combat/targeting.lua
@@ -0,0 +1,27 @@
+local function selectTarget(enemies)
+	while true do
+		-- Display enemies
+		print("Choose target:")
+		for i, enemy in ipairs(enemies) do
+			print(i .. ") " .. enemy.name .. " (" .. enemy.health .. "/" .. enemy.maxHealth .. ")")
+		end
+		print((#enemies + 1) .. ") return to action menu")
+
+		-- Get target choice
+		io.write("Target (1-" .. #enemies + 1 .. "): ")
+		local targetIndex = tonumber(io.read())
+
+		-- Validate
+		if targetIndex == #enemies + 1 then
+			return nil, nil -- Signal: they picked back
+		elseif targetIndex and targetIndex >= 1 and targetIndex <= #enemies then
+			return targetIndex, enemies[targetIndex]
+		else
+			print("invalid target!")
+		end
+	end
+end
+
+return {
+	selectTarget = selectTarget,
+}
