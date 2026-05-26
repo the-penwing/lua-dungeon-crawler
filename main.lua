9f84b1de0f6d07788b7bc7a08c3c34b72ac1578d
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-05-26 21:28:49 +1000

    make main.lua use new combat system

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ local items = require("src.items")
 
 -- Start a test combat
 local testEnemies = rooms[2].enemies -- Room 2 has 2 bats + 1 goblin
-local result = combat.combatLoop(testEnemies)
+local result = combat.loop.combatLoop(testEnemies)
 
 print("Combat result:", result)
 print("Player health:", game.player.health)
