98300a2ed00538ef3802a5e8019e5328154cb12b
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-04 14:48:06 +1000

    added prev room - is a modifided dupe of next room

--- main.lua
+++ main.lua
@@ -63,7 +63,25 @@ local function nextRoom()
 			return true
 		end
 	else
-		print("You cant go any further!")
+		print("You cant go this way!")
+		return false
+	end
+end
+local function prevRoom()
+	if rooms[game.player.currentRoom - 1] then
+		game.player.currentRoom = game.player.currentRoom - 1
+		local currentRoom = game.player.currentRoom
+		print("You move back to the previous room...")
+		ui.displayRoomDescription()
+		if #rooms[currentRoom].enemies > 0 then
+			combat.loop.combatLoop(rooms[game.player.currentRoom].enemies)
+			return true
+		else
+			print("No enemies here")
+			return true
+		end
+	else
+		print("You cant go this way!")
 		return false
 	end
 end
