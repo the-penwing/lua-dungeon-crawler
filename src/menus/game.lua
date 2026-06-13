c9f792ca5d5da33c505c9a7b79255592c165af45
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-13 20:02:11 +1000

    moved gameMenu() to its own file

---
+++ src/menus/game.lua
@@ -0,0 +1,34 @@
+local ui = require('src.ui')
+local game = require('src.game')
+local rooms = require('src.rooms')
+local function gameMenu()
+  local currentRoom = game.player.currentRoom
+  local roomDescription = rooms[currentRoom].description
+  print('--- Room ' .. currentRoom .. ': ' .. roomDescription .. ' ---')
+  print()
+  ui.displayGameState()
+  print('\nGame Menu:')
+  print('  1) Use Item')
+  print('  2) Switch Weapon')
+  print('  3) Next Room')
+  print('  4) Prev. Room')
+  print('  5) Rest')
+  print('  6) Save and Main Menu')
+  local validChoice = false
+  local choice
+  repeat
+    io.write('Enter choice (1-6): ')
+    choice = tonumber(io.read('*l'))
+    if choice >= 1 and choice <= 6 then
+      validChoice = true
+    else
+      print('Invaild choice!')
+      print('Enter 1-6')
+    end
+  until validChoice == true
+  return choice
+end
+
+return {
+  gameMenu = gameMenu,
+}
