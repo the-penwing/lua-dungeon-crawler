aa10fce36cf88e79ea0afcfc004398b981185162
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-29 00:09:37 +1000

    fixed selene warnings in save.lua

--- src/game/save.lua
+++ src/game/save.lua
@@ -2,24 +2,27 @@ local gameState = require('game.gameState')
 local json = require('libs.json')
 
 local function saveGame(filename)
-  local gameState = json.encode(gameState.player)
+  local encodedData = json.encode(gameState.player)
   local saveFile = io.open(filename, 'w')
+
   if saveFile then
-    saveFile:write(gameState)
+    saveFile:write(encodedData)
     saveFile:close()
   else
-    print('error: failed to save game!!')
+    print('Error: Failed to save game!')
   end
 end
 
 local function loadGame(filename)
   local saveFile = io.open(filename, 'r')
+
   if saveFile then
-    local gameState = json.decode(saveFile:read('*a'))
-    gameState.player = gameState
+    local loadedData = json.decode(saveFile:read('*a'))
+    saveFile:close()
+    gameState.player = loadedData
     return gameState
   else
-    print('error: failed to load game!!')
+    print('Error: Failed to load game!')
     return nil
   end
 end
