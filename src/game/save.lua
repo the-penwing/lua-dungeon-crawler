d0d68f98ad6e346b7326efc1eebf1344e792441e
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-29 04:31:07 +1000

    save files save more now

--- src/game/save.lua
+++ src/game/save.lua
@@ -2,24 +2,38 @@ local gameState = require('game.gameState')
 local json = require('libs.json')
 
 local function saveGame(filename)
-  local encodedData = json.encode(gameState.player)
+  -- Create a clean snapshot of the game data fields we want to persist
+  local saveData = {
+    player = gameState.player,
+    bossBeat = gameState.bossBeat,
+    playerAlive = gameState.playerAlive,
+  }
+
+  local encodedData = json.encode(saveData)
   local saveFile = io.open(filename, 'w')
 
   if saveFile then
     saveFile:write(encodedData)
     saveFile:close()
+    print('Game saved successfully!')
   else
     print('Error: Failed to save game!')
   end
 end
-
 local function loadGame(filename)
   local saveFile = io.open(filename, 'r')
 
   if saveFile then
-    local loadedData = json.decode(saveFile:read('*a'))
+    local decodedData = json.decode(saveFile:read('*a'))
     saveFile:close()
-    gameState.player = loadedData
+
+    -- Safely restore the state pieces individually
+    if decodedData then
+      gameState.player = decodedData.player or gameState.player
+      gameState.bossBeat = decodedData.bossBeat or false
+      gameState.playerAlive = decodedData.playerAlive ~= false -- default to true if missing
+    end
+
     return gameState
   else
     print('Error: Failed to load game!')
